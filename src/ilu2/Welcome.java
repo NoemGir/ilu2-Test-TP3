ceed164a904e6a5a95243048344d450fb765733d
Noemie Gireaud <grn4213a@ens.fsi>
2023-04-21 15:03:50 +0200

    EX_2 corrigé

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -4,9 +4,9 @@ public class Welcome {
 
 	public static String welcome(String input) {
 		StringBuilder message = new StringBuilder("Hello, ");
-	//	if(input == null || input.trim().equals("")) {
-	//		return message.append("my friend").toString();
-	//	}
+		if(input == null || input.trim().equals("")) {
+			return message.append("my friend").toString();
+		}
 			String firstLetter = input.substring(0, 1);
 			String restOfWord = input.substring(1, input.length());
 			return message.append(firstLetter.toUpperCase() + restOfWord).toString();
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -20,5 +20,18 @@ nom). */
 		assertEquals("Hello, M", Welcome.welcome("M") );
 	}
 	
+	/*EX_2 : Gestion des null et des chaînes vides
+Si on passe en paramètre une chaîne de caractère vide, nulle ou blanche (que des
+espaces), alors retourner “Hello, my friend”.
+Par exemple, si on lui passe “ “, elle retourne “Hello, my friend”.
+Indice : la méthode “trim()” de la classe String peut vous simplifier la vie */
 	
+	@Test
+	void test_EX_2() {
+		assertEquals(Welcome.welcome("   "), "Hello, my friend");
+		assertEquals(Welcome.welcome(""), "Hello, my friend");
+		assertEquals(Welcome.welcome(null), "Hello, my friend");
+	}
+	
+
 }
