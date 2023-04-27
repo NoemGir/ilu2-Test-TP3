c033446f310da99c9aa481c8bdee165be72c7100
Noémie <noemie.gireaud@gmail.com>
2023-04-27 11:56:50 +0200

    EX_9

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -16,7 +16,6 @@ public class Welcome {
 			//cas ou il y au moins un nom en majuscule
 			return casMajuscule(message,messageUpperCase,input).toString();
 		}
-		System.out.println("nouveau message 3 : " + message);
 		return message.toString();
 	}
 	/* la fonction triDesPrenoms met les prenoms dans le StringBuilder associé ( en fonction des majuscule ) 
@@ -34,12 +33,12 @@ public class Welcome {
 		}
 		retirerDerniereVirgule(messageUpperCase);
 		retirerDerniereVirgule(message);
-		ajouterAnd(messageUpperCase);
-		ajouterAnd(message);
+		ajouterAnd(messageUpperCase, "AND");
+		ajouterAnd(message, "and");
 		return hasUpperCase;
 	}
 	
-	private static void ajouterAnd(StringBuilder message) {
+	private static void ajouterAnd(StringBuilder message, String and) {
 		String[] separation = message.toString().split("(?<=,)");
 		if(separation.length > 2) {
 			StringBuilder avantDernierNom = new StringBuilder(separation[separation.length-2]);
@@ -47,7 +46,8 @@ public class Welcome {
 			int length = avantDernierNom.length();
 			avantDernierNom.delete(length-1, length);
 			//ajouter and
-			avantDernierNom.append(" and");
+			avantDernierNom.append(" ");
+			avantDernierNom.append(and);
 			//reassambler message
 			separation[separation.length-2]=avantDernierNom.toString();
 			message.setLength(0);
@@ -58,8 +58,59 @@ public class Welcome {
 	}
 
 	private static void ajoutPrenom(StringBuilder message, String nom) {
-		message.append(mettrePremiereLettreMaj(nom));
-		message.append(", ");
+		nom = mettrePremiereLettreMaj(nom);
+
+		StringBuilder aReproduire = new StringBuilder(" ");
+		aReproduire.append(nom);
+		// verifier si e prenom est déja présent dans la liste
+		if(message.toString().contains(aReproduire)) {
+			casPrenomDejaPresent(message, aReproduire);
+		}
+		// si le prenom n'est pas présent, on l'ajoute
+		else {
+			message.append(nom);
+			message.append(", ");
+		}
+	}
+	
+	private static void casPrenomDejaPresent(StringBuilder message, StringBuilder aReproduire) {
+		int index = message.indexOf(aReproduire.toString())+aReproduire.length()-1;
+		StringBuilder debutMessage = new StringBuilder(message.toString().substring(0, index+1));
+	
+		StringBuilder finMessage;
+		int debutFinMessage = index+1;
+		
+		// on vérifie si le prénom est a déja un nombre d'occurence associé 
+		if(message.charAt(index+1) == '(') {
+			StringBuilder nouvelleValeur = donnerNouvelleValeurParentheses(message, index);
+			debutMessage.append(nouvelleValeur);
+			debutFinMessage += nouvelleValeur.length();
+		}
+		// si il n'a pas de nombre d'occurence associé alors il suffit d'initaliser son nombre d'occurence a 2
+		else {
+			debutMessage.append("(x2)");
+		}
+		finMessage = new StringBuilder(message.toString().substring(debutFinMessage, message.length()));
+		
+		message.setLength(0);
+		message.append(debutMessage);
+		message.append(finMessage);
+	}
+	
+	// donnerNouvelleValeurParentheses retourne un StringBuilder qui correspond 
+	//aux parentheses avec le nouveau nombre d'occurence a l'interieur
+	private static StringBuilder donnerNouvelleValeurParentheses(StringBuilder message, int index) {
+		StringBuilder resteMessage = new StringBuilder(message.toString().substring(index+1, message.length()));
+		StringBuilder strNum = new StringBuilder(resteMessage.toString().substring(2, resteMessage.indexOf(")")));
+		
+		int numero = Integer.parseInt(String.valueOf(strNum));
+		numero++;
+		
+		StringBuilder parentheses = new StringBuilder("(x");
+		parentheses.append(numero);
+		parentheses.append(")");
+		
+		return parentheses;
 	}
 	
 	private static StringBuilder casMajuscule(StringBuilder message,StringBuilder messageUpperCase, String input) {
@@ -67,7 +118,7 @@ public class Welcome {
 		// cas ou plusieurs nom son donnés dont l'un est une majuscule
 		if (input.contains(",")) {
 			message.append(". AND ");
-			return message.append(messageUpperCase.toString().toUpperCase());
+			return message.append(messageUpperCase.toString());
 		}
 		// cas ou juste un nom a été donné
 		return messageUpperCase;
@@ -75,10 +126,16 @@ public class Welcome {
 	
 	private static void retirerDerniereVirgule(StringBuilder str) {
 		int length = str.length();
-		str.delete(length-2, length);
+		if(str.lastIndexOf(",") == length-2) {
+			str.delete(length-2, length);
+		}
+		else {
+			str.delete(length-1, length);
+		}
 	}
 	
 	private static String mettrePremiereLettreMaj(String nom) {
+		//  sb.setCharAt(pos, ch);
 		String firstLetter = nom.substring(0, 1);
 		String restOfWord = nom.substring(1, nom.length());
 		StringBuilder resultat = new StringBuilder(firstLetter.toUpperCase());
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -110,8 +110,27 @@ Par exemple, si on donne en entrée “bob , amy ”, alors répondre “Hello, 
 		assertEquals("Hello, Bob, Amy and Jerry", Welcome.welcome("   bob,   amy,  jerry"));
 		assertEquals("Hello, Bob and Jerry. AND HELLO, AMY AND JACK !", Welcome.welcome(" bob  , AMY,  jerry, JACK "));
 	}
-}
+	
+	/*EX_9 : Indiquer le nombre de fois qu’un nom apparaît dans la chaîne de caractères.
+Si un nom est présent plusieurs fois dans la chaîne passée en paramètre, il n’est salué
+qu’une seule fois, mais le nombre de fois qu’il est présent doit être précisé dans la réponse.
+Par exemple, si on donne “bob, JERRY, amy, bob, JERRY, bob”, alors répondre “Hello, Bob
+(x3), and Amy. AND HELLO JERRY (x2)”.
+indice : Vous pouvez utiliser deux tableaux, le premier contenant la liste des noms (distincts)
+passés en paramètre, et le second contenant le nombre de fois que le nom correspondant
+apparaît. Ces deux tableaux sont ensuite passés en paramètre à une nouvelle méthode
+privée qui fait l’affichage proprement dit.
+	  */
 
+	@Test
+	void test_EX_9() {
+		assertEquals("Hello, Bob(x3) and Amy. AND HELLO, JERRY(x2) !", Welcome.welcome("bob, JERRY, amy, bob, JERRY, bob"));
+		assertEquals("Hello, Bob(x2), Jerry(x3) and Jack(x2). AND HELLO, AMY(x2), JACK AND BOB !", Welcome.welcome(" bob, AMY, jerry, JACK, BOB, jack, jack, bob, jerry, AMY, jerry"));
+		assertEquals("Hello, Bob(x15)", Welcome.welcome(" bob, bob, bob, bob, Bob, Bob, bob, Bob, Bob, bob, bob ,bob, bob, bob, bob"));
+		assertEquals("Hello, Hello(x3). AND HELLO, HELLO(x2) !", Welcome.welcome(" hello, hello, HELLO, Hello, HELLO"));
+		assertEquals("Hello, H(x2), B(x2) and C(x2). AND HELLO, H(x2) !", Welcome.welcome(" h, H, b, H, b, c  , c, h"));
+	}
+}
 
 
 
